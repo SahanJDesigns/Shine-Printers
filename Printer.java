7f0b0b59bbc86e87fc28c476e91b1e3823a90c9c
Thimira-Sahan <sahanjdesigns@gmail.com>
2024-04-03 17:39:57 +0530

    attempt 11

--- Printer.java
+++ Printer.java
@@ -1,9 +1,10 @@
 class Printer extends Thread {
     PrintJob printJob;
     String name;
+
     // Method to receive a print job
-    public void getPrintJob(PrintJob printJob){
-        this.printJob = printJob;
+    public synchronized void getPrintJob(SharedQueue sharedQueue){
+        this.printJob = sharedQueue.getJob();
     }
 
     // Method to print a PrintJob
