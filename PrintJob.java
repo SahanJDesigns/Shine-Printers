e4b3f4c970562eb2859832b16bfff07a33f86e49
Thimira-Sahan <sahanjdesigns@gmail.com>
2024-04-03 16:42:29 +0530

    attempt 06

--- PrintJob.java
+++ PrintJob.java
@@ -1,11 +1,16 @@
 public class PrintJob {
-    TextFile content
-    public synchronized PrintJob(Object file) throws TypeNotSupportedException{
-        if(file instanceof TextFile){
-            TextFile textFile = (TextFile) file;
-            
+    private TextFile file;
+
+    public PrintJob(Object file) throws TypeNotSupportedException {
+        if (file instanceof TextFile) {
+            this.file = (TextFile) file;
         } else {
             throw new TypeNotSupportedException();
         }
     }
+
+    public synchronized void Print(){
+        System.out.println(file.getContent());
+    }
+    
 }
