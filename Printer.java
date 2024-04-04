e45418b9a1bc9ead5c928f7684c205e5a770083a
Dineth Randula <dinethrandula2001@gmail.com>
2024-04-03 17:42:25 -0700

    -

--- Printer.java
+++ Printer.java
@@ -1,6 +1,7 @@
 class Printer extends Thread {
     PrintJob printJob;
     String name;
+    
     // Method to receive a print job
     public void getPrintJob(PrintJob printJob){
         this.printJob = printJob;
